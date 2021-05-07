adda58dc6f973a8ab2ed13542c116dac52074f20
e0marina <emilymarinaro@gmail.com>
2021-05-06 17:36:22 -0700

    added twilio text script but not receiving on my cell yet

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+//const bodyParser = require('body-parser');
+require('dotenv').config();
 const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
@@ -17,6 +19,23 @@ if (process.env.NODE_ENV === 'production') {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+//send text
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+// Send the text message.
+client.messages
+  .create({
+    messagingServiceSid: 'MG01c49fa43b1e039eecce09e79424edd3',
+    to: process.env.YOUR_NUMBER,
+    from: process.env.YOUR_TWILIO_NUMBER,
+    body: 'Hello from Twilio!',
+  })
+  .then((message) => console.log(message.sid))
+  .done();
 
 db.once('open', () => {
   app.listen(PORT, () => {
---
+++ server/utils/text.js
@@ -0,0 +1,15 @@
+// const twilio = require('twilio');
+
+// const client = new twilio(
+//   process.env.TWILIO_ACCOUNT_SID,
+//   process.env.TWILIO_AUTH_TOKEN
+// );
+
+// // Send the text message.
+// client.messages.create({
+//   to: process.env.YOUR_NUMBER,
+//   from: process.env.YOUR_TWILIO_NUMBER,
+//   body: 'Hello from Twilio!',
+// });
+
+// module.exports = client;
